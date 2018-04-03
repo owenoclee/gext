cfa1963c593148fbd6b9303ede2503402f3856aa
Owen <oo310@kent.ac.uk>
2018-04-03 21:44:23 +0100

    Add panic handler to deal with panics in controllers

--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -10,6 +11,8 @@ import (
 
 func initRouter(db *sql.DB) *httprouter.Router {
 	router := httprouter.New()
+	router.PanicHandler = panicHandler
+
 	router.POST("/posts", controllers.StorePost.Handler(db))
 	router.OPTIONS("/posts", corsHandler)
 	router.GET("/threads/:id", controllers.ShowThread.Handler(db))
@@ -24,4 +27,9 @@ func corsHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
+func panicHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
+	log.Printf("panic handling http %v request to '%v':\n%v\n", r.Method, r.RequestURI, err)
+	w.WriteHeader(500)
 }
